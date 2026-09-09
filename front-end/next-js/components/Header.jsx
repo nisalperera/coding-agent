4d6b4d1e34d77c525c4e25a8564a229b9c4d8c79
Nisal Perera <chinthanapereranisal@gmail.com>
2026-09-09 16:57:07 +0530

    feat(compose): add local MySQL migration workflow

--- front-end/next-js/components/Header.jsx
+++ front-end/next-js/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header({
                 <div className="flex min-w-0 items-center gap-2">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
-                        src="/assets/logo.jpeg"
+                        src="/public/images/logo.jpeg"
                         alt="Nisal's Coding Agent logo"
                         className="h-8 w-8 shrink-0 rounded-lg object-cover"
                     />
